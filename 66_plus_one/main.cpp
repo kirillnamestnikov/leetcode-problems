6fe4804020629f5b8d1564b84abeea7175ddb7e0
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-22 18:11:43 +0300

    plus_one

---
+++ 66_plus_one/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int size = digits.size();
+        int pos = size - 1;
+        while ((pos >= 0) && (digits[pos] == 9))
+        {
+            digits[pos] = 0;
+            pos--;
+        }
+        if (pos < 0)
+        {
+            digits.insert(digits.begin(), 1);
+        }
+        else
+        {
+            digits[pos]++;
+        }
+        return digits;
+    }
+
+};
