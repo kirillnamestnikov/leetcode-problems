458dd6d99d4d0d00407ee60285a87666dd77a0dc
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-07-15 11:02:59 +0300

    hamming_distance

---
+++ 461_hamming_distance/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count = 0;
+        int result = x ^ y;
+        while (result > 0)
+        {
+            if ((result & 1) > 0)
+            {
+                ++count;
+            }
+            result = result>>1;
+        }
+        return count;
+    }
+};
