b2aebcbc75afc4789558f43280a3b7c64987fc5b
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-07-28 16:30:52 +0300

    reverse_string

---
+++ 344_reverse_string/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right)
+        {
+            std::swap(s[left], s[right]);
+            ++left;
+            --right;
+        }
+    }
+};
