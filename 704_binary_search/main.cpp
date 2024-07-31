c238d4b25decb71d0d2aa58936aabba220d8fd7a
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-07-31 17:04:58 +0300

    binary_search

---
+++ 704_binary_search/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int mid = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
