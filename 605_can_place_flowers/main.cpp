c62997a70a6731e71cb5871e2b79ec8915180ce0
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-08-21 10:39:59 +0300

    can_place_flowers

---
+++ 605_can_place_flowers/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for (size_t i = 0; i < flowerbed.size(); ++i)
+        {
+            bool left = (i == 0) || (flowerbed[i - 1] == 0);
+            bool right = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
+            if (left && right && (flowerbed[i] == 0))
+            {
+                flowerbed[i] = 1;
+                --n;
+            }
+            if (n <= 0)
+            {
+                return true;
+            }
+        }
+        return n <= 0;
+    }
+};
