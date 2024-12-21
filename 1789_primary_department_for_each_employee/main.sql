0b31e96478947f334c8c2248f13ec24adb49110c
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-12-21 19:18:49 +0300

    primary_department_for_each_employee

---
+++ 1789_primary_department_for_each_employee/main.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+select employee_id, department_id from Employee where employee_id in (select employee_id from Employee group by employee_id having count(*) = 1) or primary_flag = 'Y';
